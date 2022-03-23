711180ff7d7f013683a532fe2766379852b26264
Cooper Myers <60561014+cooperalanmyers@users.noreply.github.com>
2022-03-23 18:37:09 -0400

    Update ftp_server.py

--- ftp_server.py
+++ ftp_server.py
@@ -79,7 +79,6 @@ def stor():
 def dataConnection():
     new_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    time.sleep(3)
     # new_server_socket.connect((server_ip, int(new_port_number)))
     new_server_socket.connect((server_ip, int(new_port_number)))
 
