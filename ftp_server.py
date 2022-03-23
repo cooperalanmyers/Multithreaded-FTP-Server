2d8db2d52ef9f6f1539da3b22c64696807e04665
Cooper Myers <60561014+cooperalanmyers@users.noreply.github.com>
2022-03-23 17:23:39 -0400

    Update ftp_server.py

--- ftp_server.py
+++ ftp_server.py
@@ -34,7 +34,7 @@ def command_menu(connection_socket):
                 new_port_number = data
                 new_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 new_server_socket.connect((connectIP, int(new_port_number)))
-        
+                print("New Port Number is stored!\nPORT: " + new_port_number)
            # Reset Data to Loop Through Again
            data = None
     
