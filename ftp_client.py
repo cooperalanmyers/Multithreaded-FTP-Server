e1eef6cc7a6f0ad0251e4acea1bda4beede3c1d7
Cooper Myers <60561014+cooperalanmyers@users.noreply.github.com>
2022-03-23 17:20:14 -0400

    Update ftp_client.py

--- ftp_client.py
+++ ftp_client.py
@@ -34,8 +34,7 @@ def connect():
     if connectTemp[0].upper() == paramOne: 
         client_socket.connect((connectIP, int(connectPort)))
         client_socket.send(paramOne.encode('utf-8'))
-        
-    client_socket.send(new_server_port.encode('utf-8'))
+        client_socket.send(new_server_port.encode('utf-8'))
 
 def list_files():
     listMessage = "LIST"
