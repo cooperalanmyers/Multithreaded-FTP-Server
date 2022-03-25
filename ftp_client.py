37283d7a1ddbedaad704cce250eed0ce88269354
Cooper Myers <60561014+cooperalanmyers@users.noreply.github.com>
2022-03-25 08:26:50 -0400

    Update ftp_client.py

--- ftp_client.py
+++ ftp_client.py
@@ -38,7 +38,7 @@ def connect():
         client_socket.connect((connectIP, int(connectPort)))
         client_socket.send(paramOne.encode('utf-8'))
         
-        time.sleep(2)
+        time.sleep(3)
         
         client_socket.send(new_server_port.encode('utf-8'))
         
